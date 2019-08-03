036c8e65a3cf1e995677fe305a571086608ddea6
Cole Brown <bigswim@gmail.com>
2019-08-02 21:35:00 -0400

    Update go-libp2p, fix tests with weak RSA keys

--- namesys/ipns_resolver_validation_test.go
+++ namesys/ipns_resolver_validation_test.go
@@ -5,6 +5,8 @@ import (
 	"testing"
 	"time"
 
+	"github.com/libp2p/go-libp2p-core/test"
+
 	ds "github.com/ipfs/go-datastore"
 	dssync "github.com/ipfs/go-datastore/sync"
 	mockrouting "github.com/ipfs/go-ipfs-routing/mock"

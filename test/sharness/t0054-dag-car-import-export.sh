f23bcbf5b7e14a61e66c285092793cc1de96d5bb
Peter Rabbitson <ribasushi@protocol.ai>
2020-03-04 16:11:14 +0100

    Dag import functionality only ( no progress )
    This still works over "loosely defined" .car files
    Please refer to the sharness tests for extra info
    
    We can tighten this up if the sentiment is "Postel was wrong"

--- test/sharness/t0054-dag-car-import-export.sh
+++ test/sharness/t0054-dag-car-import-export.sh
@@ -4,51 +4,37 @@
 test_description="Test car file import/export functionality"
 
 . lib/test-lib.sh
-<<<<<<< HEAD
 export -f ipfsi
 
 set -o pipefail
 
 tar -C ../t0054-dag-car-import-export-data/ --strip-components=1 -Jxf ../t0054-dag-car-import-export-data/test_dataset_car_v0.tar.xz
-tab=$'\t'
-
-test_cmp_sorted() {
-  # use test_cmp to dump out the unsorted file contents as a diff
-  [[ "$( sort "$1" | sha256sum )" == "$( sort "$2" | sha256sum )" ]] \
-    || test_cmp "$1" "$2"
-}
-export -f test_cmp_sorted
 
 reset_blockstore() {
   node=$1
-
-  ipfsi "$node" pin ls --quiet --type=recursive | ipfsi "$node" pin rm &>/dev/null
-  ipfsi "$node" repo gc &>/dev/null
+  ipfsi $1 pin ls --quiet --type=recursive | ipfsi $1 pin rm &>/dev/null
+  ipfsi $1 repo gc &>/dev/null
 
   test_expect_success "pinlist empty" '
-    [[ -z "$( ipfsi $node pin ls )" ]]
+    test -z "$( ipfsi $1 pin ls )"
   '
   test_expect_success "nothing left to gc" '
-    [[ -z "$( ipfsi $node repo gc )" ]]
+    test -z "$( ipfsi $1 repo gc )"
   '
 }
 
 # hammer with concurrent gc to ensure nothing clashes
 do_import() {
-  node="$1"; shift
-  (
-      touch spin.gc
+  node=$1; shift
 
-      while [[ -e spin.gc ]]; do ipfsi "$node" repo gc &>/dev/null; done &
-      while [[ -e spin.gc ]]; do ipfsi "$node" repo gc &>/dev/null; done &
+  bash -c "while [[ -e spin.gc ]]; do ipfsi $node repo gc >>gc_out 2>&1; done" & gc1_pid=$!
+  bash -c "while [[ -e spin.gc ]]; do ipfsi $node repo gc >>gc_out 2>&1; done" & gc2_pid=$!
 
-      ipfsi "$node" dag import "$@" 2>&1 && ipfsi "$node" repo verify &>/dev/null
-      result=$?
+  ipfsi $node dag import "$@"
 
-      rm -f spin.gc &>/dev/null
-      wait || true  # work around possible trigger of a bash bug on overloaded circleci
-      exit $result
-  )
+  rm spin.gc
+  wait $gc1_pid
+  wait $gc2_pid
 }
 
 run_online_imp_exp_tests() {
@@ -56,55 +42,25 @@ run_online_imp_exp_tests() {
   reset_blockstore 0
   reset_blockstore 1
 
-  cat > basic_import_expected <<EOE
-Pinned root${tab}bafkqaaa${tab}success
-Pinned root${tab}bafy2bzaceaxm23epjsmh75yvzcecsrbavlmkcxnva66bkdebdcnyw3bjrc74u${tab}success
-Pinned root${tab}bafy2bzaced4ueelaegfs5fqu4tzsh6ywbbpfk3cxppupmxfdhbpbhzawfw5oy${tab}success
-EOE
+  echo -e "Pinned root\tbafkqaaa\tsuccess (root specified in .car header without available data)" > basic_import_expected
+  echo -e "Pinned root\tbafy2bzaceaxm23epjsmh75yvzcecsrbavlmkcxnva66bkdebdcnyw3bjrc74u\tsuccess" >> basic_import_expected
+  echo -e "Pinned root\tbafy2bzaced4ueelaegfs5fqu4tzsh6ywbbpfk3cxppupmxfdhbpbhzawfw5oy\tsuccess" >> basic_import_expected
 
-  cat >naked_root_import_json_expected <<EOE
-{"Root":{"Cid":{"/":"bafy2bzaceaxm23epjsmh75yvzcecsrbavlmkcxnva66bkdebdcnyw3bjrc74u"},"PinErrorMsg":""}}
-{"Root":{"Cid":{"/":"bafy2bzaced4ueelaegfs5fqu4tzsh6ywbbpfk3cxppupmxfdhbpbhzawfw5oy"},"PinErrorMsg":""}}
-EOE
-
-
+  touch spin.gc
   test_expect_success "basic import" '
     do_import 0 \
       ../t0054-dag-car-import-export-data/combined_naked_roots_genesis_and_128.car \
       ../t0054-dag-car-import-export-data/lotus_testnet_export_128_shuffled_nulroot.car \
       ../t0054-dag-car-import-export-data/lotus_devnet_genesis_shuffled_nulroot.car \
-    > basic_import_actual
+    | sort > basic_import_actual
   '
 
+  # FIXME - the fact we reliably fail this is indicative of some sort of race...
+  test_expect_failure "concurrent GC did not manage to find anything" '
+    ! [[ -s gc_out ]]
+  '
   test_expect_success "basic import output as expected" '
-    test_cmp_sorted basic_import_expected basic_import_actual
-  '
-
-  test_expect_success "basic fetch+export 1" '
-    ipfsi 1 dag export bafy2bzaced4ueelaegfs5fqu4tzsh6ywbbpfk3cxppupmxfdhbpbhzawfw5oy > reexported_testnet_128.car
-  '
-  test_expect_success "export of shuffled testnet export identical to canonical original" '
-    test_cmp reexported_testnet_128.car ../t0054-dag-car-import-export-data/lotus_testnet_export_128.car
-  '
-
-  test_expect_success "basic fetch+export 2" '
-    ipfsi 1 dag export bafy2bzaceaxm23epjsmh75yvzcecsrbavlmkcxnva66bkdebdcnyw3bjrc74u > reexported_devnet_genesis.car
-  '
-  test_expect_success "export of shuffled devnet export identical to canonical original" '
-    test_cmp reexported_devnet_genesis.car ../t0054-dag-car-import-export-data/lotus_devnet_genesis.car
-  '
-
-  test_expect_success "pinlist on node1 still empty" '
-    [[ -z "$( ipfsi 1 pin ls )" ]]
-  '
-
-  test_expect_success "import/pin naked roots only, relying on local blockstore having all the data" '
-    ipfsi 1 dag import --enc=json ../t0054-dag-car-import-export-data/combined_naked_roots_genesis_and_128.car \
-      > naked_import_result_json_actual
-  '
-
-  test_expect_success "naked import output as expected" '
-    test_cmp_sorted naked_root_import_json_expected naked_import_result_json_actual
+    test_cmp basic_import_expected basic_import_actual
   '
 
   reset_blockstore 0
@@ -113,29 +69,37 @@ EOE
   mkfifo pipe_testnet
   mkfifo pipe_devnet
 
-  test_expect_success "fifo import" '
-    (
-        cat ../t0054-dag-car-import-export-data/lotus_testnet_export_128_shuffled_nulroot.car > pipe_testnet &
-        cat ../t0054-dag-car-import-export-data/lotus_devnet_genesis_shuffled_nulroot.car > pipe_devnet &
+  # test that ipfs correctly opens both pipes and deleting them doesn't interfere with cleanup
+  bash -c '
+    sleep 1
+    cat ../t0054-dag-car-import-export-data/lotus_testnet_export_128_shuffled_nulroot.car > pipe_testnet & cat1_pid=$!
+    cat ../t0054-dag-car-import-export-data/lotus_devnet_genesis_shuffled_nulroot.car > pipe_devnet & cat2_pid=$!
 
-        do_import 0 \
-          pipe_testnet \
-          pipe_devnet \
-          ../t0054-dag-car-import-export-data/combined_naked_roots_genesis_and_128.car \
-        > basic_fifo_import_actual
-        result=$?
-
-        wait || true	# work around possible trigger of a bash bug on overloaded circleci
-        exit "$result"
-    )
-  '
-
-  test_expect_success "remove fifos" '
     rm pipe_testnet pipe_devnet
+
+    # extra safety valve to kill the cat processes in case something goes wrong
+    bash -c "sleep 60; kill $cat1_pid $cat2_pid 2>/dev/null" &
+  ' &
+
+  touch spin.gc
+  test_expect_success "fifo import" '
+    do_import 0 \
+      pipe_testnet \
+      pipe_devnet \
+      ../t0054-dag-car-import-export-data/combined_naked_roots_genesis_and_128.car \
+    | sort > basic_fifo_import_actual
+  '
+  # FIXME - the fact we reliably fail this is indicative of some sort of race...
+  test_expect_failure "concurrent GC did not manage to grab anything" '
+    ! [[ -s gc_out ]]
   '
 
   test_expect_success "fifo-import output as expected" '
-    test_cmp_sorted basic_import_expected basic_fifo_import_actual
+    test_cmp basic_import_expected basic_fifo_import_actual
+  '
+
+  test_expect_success "fifos no longer present" '
+    ! [[ -e pipe_testnet ]] && ! [[ -e pipe_devnet ]]
   '
 }
 
@@ -157,7 +121,7 @@ test_init_ipfs
 
 
 test_expect_success "basic offline export of 'getting started' dag works" '
-  ipfs dag export "$HASH_WELCOME_DOCS" >/dev/null
+  ipfs dag export QmS4ustL54uo8FzR9455qaxZwuMiUhyvMcX9Ba8nUH4uVv >/dev/null
 '
 
 
@@ -166,20 +130,20 @@ test_expect_success "basic offline export of nonexistent cid" '
   ! ipfs dag export QmYwAPJXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX 2> offline_fetch_error_actual >/dev/null
 '
 test_expect_success "correct error" '
-  test_cmp_sorted offline_fetch_error_expected offline_fetch_error_actual
+  test_cmp offline_fetch_error_expected offline_fetch_error_actual
 '
 
 
-cat >multiroot_import_json_expected <<EOE
-{"Root":{"Cid":{"/":"bafy2bzaceb55n7uxyfaelplulk3ev2xz7gnq6crncf3ahnvu46hqqmpucizcw"},"PinErrorMsg":""}}
-{"Root":{"Cid":{"/":"bafy2bzacebedrc4n2ac6cqdkhs7lmj5e4xiif3gu7nmoborihajxn3fav3vdq"},"PinErrorMsg":""}}
-{"Root":{"Cid":{"/":"bafy2bzacede2hsme6hparlbr4g2x6pylj43olp4uihwjq3plqdjyrdhrv7cp4"},"PinErrorMsg":""}}
+cat >multiroot_import_expected <<EOE
+{"Root":{"Cid":{"/":"bafy2bzaceb55n7uxyfaelplulk3ev2xz7gnq6crncf3ahnvu46hqqmpucizcw"},"PresentInImport":true,"PinErrorMsg":""}}
+{"Root":{"Cid":{"/":"bafy2bzacebedrc4n2ac6cqdkhs7lmj5e4xiif3gu7nmoborihajxn3fav3vdq"},"PresentInImport":true,"PinErrorMsg":""}}
+{"Root":{"Cid":{"/":"bafy2bzacede2hsme6hparlbr4g2x6pylj43olp4uihwjq3plqdjyrdhrv7cp4"},"PresentInImport":true,"PinErrorMsg":""}}
 EOE
 test_expect_success "multiroot import works" '
-  ipfs dag import --enc=json ../t0054-dag-car-import-export-data/lotus_testnet_export_256_multiroot.car > multiroot_import_json_actual
+  ipfs dag import --enc=json ../t0054-dag-car-import-export-data/lotus_testnet_export_256_multiroot.car | sort > multiroot_import_actual
 '
 test_expect_success "multiroot import expected output" '
-  test_cmp_sorted multiroot_import_json_expected multiroot_import_json_actual
+  test_cmp multiroot_import_expected multiroot_import_actual
 '
 
 
@@ -190,34 +154,21 @@ test_expect_success "pin-less import works" '
     > no-pin_import_actual
 '
 test_expect_success "expected silence on --pin-roots=false" '
-  test_cmp /dev/null no-pin_import_actual
+  ! [[ -s no-pin_import_actual ]]
 '
 
 
+cat >naked_root_import_expected <<EOE
+{"Root":{"Cid":{"/":"bafy2bzaceaxm23epjsmh75yvzcecsrbavlmkcxnva66bkdebdcnyw3bjrc74u"},"PresentInImport":false,"PinErrorMsg":""}}
+{"Root":{"Cid":{"/":"bafy2bzaced4ueelaegfs5fqu4tzsh6ywbbpfk3cxppupmxfdhbpbhzawfw5oy"},"PresentInImport":false,"PinErrorMsg":""}}
+EOE
 test_expect_success "naked root import works" '
   ipfs dag import --enc=json ../t0054-dag-car-import-export-data/combined_naked_roots_genesis_and_128.car \
-    > naked_root_import_json_actual
+  | sort > naked_root_import_actual
 '
 test_expect_success "naked root import expected output" '
-   test_cmp_sorted naked_root_import_json_expected naked_root_import_json_actual
-=======
-
-test_init_ipfs
-
-
-echo "Error: merkledag: not found (currently offline, perhaps retry after attaching to the network)" > offline_fetch_error_expected
-
-test_expect_success "basic offline export of nonexistent cid" '
-  ! ipfs dag export QmYwAPJXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX 2> offline_fetch_error_actual
+   test_cmp naked_root_import_expected naked_root_import_actual
 '
 
-test_expect_success "correct error" '
-  test_cmp offline_fetch_error_expected offline_fetch_error_actual
-'
-
-test_expect_success "basic offline export of 'getting started' dag" '
-  ipfs dag export QmS4ustL54uo8FzR9455qaxZwuMiUhyvMcX9Ba8nUH4uVv >/dev/null
->>>>>>> Dag export command, silent, no progress
-'
 
 test_done

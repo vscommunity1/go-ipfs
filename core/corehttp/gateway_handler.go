e765567ed63e7bc02b1ef145fb7666166ab33810
JP Hastings-Spital <jphastings@gmail.com>
2020-05-09 00:16:25 +0100

    Gateway renders pretty 404 pages if available
    In the same way that an `index.html` file is rendered, if one is present, when the
    requested path is a directory, now an `ipfs-404.html` file is rendered if
    the requested file is not present within the specified IPFS object.
    
    `ipfs-404.html` files are looked for in the directory of the requested path and each
    parent until one is found, falling back on the well-known 404 error message.
    
    License: MIT
    Signed-off-by: JP Hastings-Spital <jphastings@gmail.com>

--- core/corehttp/gateway_handler.go
+++ core/corehttp/gateway_handler.go
@@ -342,6 +342,10 @@ func (i *gatewayHandler) getOrHeadHandler(w http.ResponseWriter, r *http.Request
 		return
 	}
 
+	if i.servePretty404IfPresent(w, r, parsedPath) {
+		return
+	}
+
 	// storage for directory listing
 	var dirListing []directoryItem
 	dirit := dir.Entries()

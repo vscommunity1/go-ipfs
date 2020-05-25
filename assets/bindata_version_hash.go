d0ab9bd7ef1fa8fab3678b6c9008fb8668b45535
Peter Rabbitson <ribasushi@protocol.ai>
2020-05-25 22:10:51 +0200

    Add generated comment

--- assets/bindata_version_hash.go
+++ assets/bindata_version_hash.go
@@ -1,7 +1,4 @@
-<<<<<<< HEAD
 // File generated together with 'bindata.go' when running `go generate .` DO NOT EDIT. (@generated)
-=======
->>>>>>> Include the git blob id of the dir-index bundle in the ETag
 package assets
 
 const (

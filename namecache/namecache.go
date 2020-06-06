CEPBEP/ipfs

namecache: initialize blockstore
License: MIT
Signed-off-by: vyzo <vyzo@hackzen.org>

namecache/namecache.go
@@ -48,12 +48,13 @@ type nameCache struct {
 	mx      sync.Mutex
 }
 
-func NewNameCache(ctx context.Context, nsys namesys.NameSystem, pinning pin.Pinner, dag merkledag.DAGService) NameCache {
+func NewNameCache(ctx context.Context, nsys namesys.NameSystem, pinning pin.Pinner, dag merkledag.DAGService, bstore bstore.GCBlockstore) NameCache {
 	return &nameCache{
 		ctx:     ctx,
 		nsys:    nsys,
 		pinning: pinning,
 		dag:     dag,
+		bstore:  bstore,
 		follows: make(map[string]func()),
 	}
 }

vscommunity1/go-ipfs

namecache/namecache.go
@@ -48,12 +48,13 @@ type nameCache struct {
 	mx      sync.Mutex
 }
 
-func NewNameCache(ctx context.Context, nsys namesys.NameSystem, pinning pin.Pinner, dag ipld.DAGService) NameCache {
+func NewNameCache(ctx context.Context, nsys namesys.NameSystem, pinning pin.Pinner, dag ipld.DAGService, bstore bstore.GCBlockstore) NameCache {
 	return &nameCache{
 		ctx:     ctx,
 		nsys:    nsys,
 		pinning: pinning,
 		dag:     dag,
+		bstore:  bstore,
 		follows: make(map[string]func()),
 	}
 }